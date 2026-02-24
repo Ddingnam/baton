883e6412c3ce08ecc730cdf72d7c0c6795c5d0e7
neuli30 <haneul010127@gmail.com>
2026-02-24 16:01:00 +0900

    크밋

--- src/main/java/com/sp/app/service/TradeServiceImpl.java
+++ src/main/java/com/sp/app/service/TradeServiceImpl.java
@@ -1,40 +1,5 @@
 package com.sp.app.service;
 
-import java.util.List;
-import java.util.Map;
-
-import org.springframework.stereotype.Service;
-
-import com.sp.app.model.JobPosting;
-
-import lombok.RequiredArgsConstructor;
-
-@Service 
-@RequiredArgsConstructor
-public class TradeServiceImpl implements JobPostingService {
+public class TradeServiceImpl implements TradeService {
 	
-	@Override
-	public void insertPosting(JobPosting dto) throws Exception {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public List<JobPosting> listPosting(Map<String, Object> map) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public int dataCount(Map<String, Object> map) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public JobPosting findById(long postingIdx) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-    // ... 내용
 }
